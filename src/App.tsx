a20914040bea9762131064b9fa1ae50c496679a1
Yoshihiro Tanaka <contact@cordea.jp>
2021-10-17 14:37:01 +0900

    add fab

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {PlayArrow} from '@mui/icons-material';
+import {Box, Fab} from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Box sx={{
+            width: 1,
+            height: 1
+        }}>
+            <Fab sx={{
+                position: 'absolute',
+                bottom: 16,
+                right: 16
+            }}>
+                <PlayArrow/>
+            </Fab>
+        </Box>
+    );
 }
 
 export default App;
