d12d9e5b9908b3d6ad0230888fbb259273f3f007
Yoshihiro Tanaka <contact@cordea.jp>
2021-10-17 15:54:55 +0900

    define item

---
+++ src/Item.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Paper} from '@mui/material';
+
+const Item = () => {
+    return (
+        <Paper sx={{
+            padding: 2,
+            textAlign: 'center'
+        }}>paper</Paper>
+    );
+}
+
+export default Item;
