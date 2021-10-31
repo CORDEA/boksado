072c7af1f6b3e8ab94166d62b199ffa6c4667135
Yoshihiro Tanaka <contact@cordea.jp>
2021-10-31 12:57:08 +0900

    define action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,6 +4,7 @@ export const START = 'Start';
 export const STOP = 'Stop';
 export const SCROLL = 'Scroll';
 export const SELECT_SCORE = 'SelectScore';
+export const CHANGE_SPEED = 'ChangeSpeed';
 
 interface Start {
     type: typeof START
@@ -20,6 +21,11 @@ interface Scroll {
 interface SelectScore {
     type: typeof SELECT_SCORE
     id: number
+}
+
+interface ChangeSpeed {
+    type: typeof CHANGE_SPEED
+    query: string
 }
 
 let timer: number | null = null;
@@ -51,10 +57,17 @@ export function selectScore(dispatch: Dispatch, id: number) {
     })
 }
 
+export function changeSpeed(dispatch: Dispatch, query: string) {
+    dispatch({
+        type: CHANGE_SPEED,
+        query: query
+    })
+}
+
 function scroll() {
     return {
         type: SCROLL
     }
 }
 
-export type Actions = Start | Stop | Scroll | SelectScore;
+export type Actions = Start | Stop | Scroll | SelectScore | ChangeSpeed;
